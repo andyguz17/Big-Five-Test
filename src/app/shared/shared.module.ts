05954838087196318ba8daea3ace6ec385116ed1
Andres <andyguz17@gmail.com>
2021-08-13 03:21:43 -0400

    Right panel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SelectorComponent } from './selector/selector.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SelectorComponent],
+  declarations: [HeaderComponent, SelectorComponent, PanelComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, SelectorComponent],
+  exports: [HeaderComponent, SelectorComponent, PanelComponent],
 })
 export class SharedModule {}
