f706002ebd5b365f539866220026d5930cf76bf7
Andres <andyguz17@gmail.com>
2021-08-13 01:29:03 -0400

    openness page

--- src/app/pages/openness/openness.module.ts
+++ src/app/pages/openness/openness.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { OpennessRoutingModule } from './openness-routing.module';
 import { MainComponent } from './main/main.component';
+import { BodyComponent } from './body/body.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [MainComponent],
-  imports: [CommonModule, OpennessRoutingModule],
+  declarations: [MainComponent, BodyComponent],
+  imports: [CommonModule, OpennessRoutingModule, SharedModule],
   exports: [MainComponent],
 })
 export class OpennessModule {}
